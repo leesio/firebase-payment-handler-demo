f938487e076ef97a09b17fb7a87138e6ccd2374b
James Lees <james+github@lees.io>
2016-11-14 22:55:40 +0000

    payment ui submodule and README bump

--- app/server.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config');
-const express = require('express');
-const app = express();
-
-
-app.use(express.static('public'));
-const port = config.port || 3456;
-app.listen(port, () => console.log('Listening', port));
